b752b5aec9ea16f893f858057520ea134a64c02a
William Zhang <13520481028zhc@gmail.com>
2020-06-09 10:27:51 +0800

    Deprecate WZPP

--- src/bit/minisys/minicc/pp/WZPP.java
+++ src/bit/minisys/minicc/pp/WZPP.java
@@ -5,15 +5,16 @@ import bit.minisys.minicc.internal.util.MiniCCUtil;
 
 import java.util.ArrayList;
 
+@Deprecated
 public class WZPP implements IMiniCCPreProcessor {
     @Override
     public String run(String iFile) throws Exception {
         System.out.println("Adding declarations for MARS functions");
         ArrayList<String> src = MiniCCUtil.readFile(iFile);
         StringBuilder output = new StringBuilder();
-//        output.append("void Mars_PrintStr(char *);\n");
-//        output.append("int Mars_GetInt();\n");
-//        output.append("void Mars_PrintInt(int);\n");
+        output.append("void Mars_PrintStr(char *);\n");
+        output.append("int Mars_GetInt();\n");
+        output.append("void Mars_PrintInt(int);\n");
         for (String line : src) {
             output.append(line);
             output.append("\n");
